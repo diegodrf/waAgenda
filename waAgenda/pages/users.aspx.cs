446e7b8bb5557ad1a42c3f4b4966fc663d4b4251
Diego Faria <diego.rdfaria@gmail.com>
2022-12-07 01:56:46 -0500

    Removing unnecessary method

--- waAgenda/pages/users.aspx.cs
+++ waAgenda/pages/users.aspx.cs
@@ -10,15 +10,6 @@ namespace waAgenda
     public partial class users : System.Web.UI.Page
     {
 
-        private void UpdateControlName(string id, string newName)
-        {
-            foreach (Button component in FindControl(id).Controls)
-            {
-                component.Text = newName;
-                component.DataBind();
-            }
-        }
-
         protected void Page_Load(object sender, EventArgs e)
         {
             //UpdateControlName("DeleteButton", "Remover");
