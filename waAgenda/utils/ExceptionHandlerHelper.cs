70c07a422e9d69a05692ed11c80d3cd27475b0e6
Diego Faria <diego.rdfaria@gmail.com>
2022-12-07 02:28:25 -0500

    Created excpetion handler

---
+++ waAgenda/utils/ExceptionHandlerHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+
+namespace waAgenda.utils
+{
+    public class ExceptionHandlerHelper
+    {
+        /// <summary>
+        /// Catch custom EventArgs produced by SQLCommands
+        /// </summary>
+        /// <param name="eventArg"></param>
+        public static void Handle(dynamic eventArg, ITextControl control)
+        {
+            if (eventArg.Exception != null)
+            {
+                control.Text = $"[{eventArg.Exception.GetType()}] {eventArg.Exception.Message}";
+                eventArg.ExceptionHandled = true;
+            }
+
+            control.Text = new StringBuilder()
+            .Append("<script type=\"text/javascript\">")
+            .Append($"alert(\"[{eventArg.Exception.GetType()}]\");")
+            .Append("</script>")
+            .ToString();
+        }
+    }
+}
